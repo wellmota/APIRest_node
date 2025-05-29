8ba9c7630efc25e0c80d5e13e9edf524345428ec
wellmota <impeto.wellington@gmail.com>
2025-05-29 18:38:30 -0300

    Update .gitignore to include .env file; add dotenv dependency for environment variable management; modify database configuration to use DATABASE_URL from .env

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,15 @@
+import 'dotenv/config'
+
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not set')
+}
+
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
