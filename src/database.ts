63627de2dfb1aa23160410f9bdb4ec19dd2a8608
wellmota <impeto.wellington@gmail.com>
2025-05-29 18:56:44 -0300

    Update package dependencies to include zod; refactor database and server configurations to utilize environment variables for DATABASE_URL and PORT

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,5 @@
-import 'dotenv/config'
-
 import { knex as setupKnex, Knex } from 'knex'
+import { env } from './env'
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
@@ -9,7 +8,7 @@ if (!process.env.DATABASE_URL) {
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: process.env.DATABASE_URL,
+    filename: env.DATABASE_URL,
   },
   useNullAsDefault: true,
   migrations: {
